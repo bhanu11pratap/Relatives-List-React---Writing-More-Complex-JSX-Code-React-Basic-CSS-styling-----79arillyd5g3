373f03513b8478599ce03483b597c84093a231af
bhanu11pratap <110561155+bhanu11pratap@users.noreply.github.com>
2022-11-06 00:18:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-import React, { useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-function App{
-let relatives = ["bhanu", "prashansha", "rashi"];
-        return(
-            <div id="main">
-
+class App extends Component {
+  render() {
+    let relatives = ["bhanu", "prashansha", "rashi"];
+    return (
       <ol key="relativeList">
         {relatives.map((relative, index) => (
           <li key={`relativeListItem${index + 1}`}>{relative}</li>
         ))}
       </ol>
-            </div>
-        )
-    }
+    );
+  }
+}
 
 export default App;
